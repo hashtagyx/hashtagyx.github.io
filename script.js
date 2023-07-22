a64707d1f5553dfc801e58e881f650f31195eb90
hashtagyx <c.yongxuan@gmail.com>
2023-07-22 20:57:52 +0800

    Added d-none

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     // Show or hide the button based on scroll position
     window.addEventListener('scroll', () => {
-      if (window.scrollY > 10) {
+      if (window.scrollY > 20) {
         scrollToTopBtn.classList.remove('d-none');
       } else {
         scrollToTopBtn.classList.add('d-none');
